ae2fc8bffa4606ec9550866bd58045f7ecc4dee2
Shreyas Narsale <89839848+Shreyas-Narsale@users.noreply.github.com>
2026-05-11 11:18:34 +0530

    Create 15.Array.java

---
+++ 15.Array.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.Scanner;
+
+
+
+class Main{
+    public static void main(String args[]){
+        int[] arr = new int[10];
+        int[] arr1={10,20,30,40};
+        System.out.println(arr.length);
+
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+        int[][] jArr= new int[3][]; 
+        jArr[0]=new int[3];
+
+
+        Integer No;
+        int iNo =10;
+        No= (int)iNo;
+
+
+    }
+}
