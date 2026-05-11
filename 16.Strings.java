804a2152e4c053a12ba0a2dfb0da4811e2673d25
Shreyas Narsale <89839848+Shreyas-Narsale@users.noreply.github.com>
2026-05-11 11:19:01 +0530

    Create 16.Strings.java

---
+++ 16.Strings.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+
+
+class Main{
+    public static void main(String args[]){
+        // String str = new String("mno");
+        // str = "pqr";
+
+        // String str1="hsh";
+        // str1="shsh";
+
+        // String s= "Hello";
+        // s.concat("Demo");
+        // System.out.println(s);
+
+        String str = "Demo";
+        char[] arr = str.toCharArray();
+
+        String tr1 = new String(arr);
+    }
+}
